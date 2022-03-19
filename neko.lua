13bed73432b2e032343bf587c8c4a137c59f29bc
charlie <96938527+its-asia@users.noreply.github.com>
2022-03-18 21:23:37 -0700

    Update neko.lua

--- neko.lua
+++ neko.lua
@@ -1,27 +1,24 @@
--- FE Neko Maid Animations By Creo
--- Tail that CAN be used https://web.roblox.com/catalog/4645440842/Plain-White-Cat-Tail
--- For updates on script join this server
+-- FE Neko Maid Animations By Creo; For updates on (ORIGINAL NOT MY VERSION) script join this server
 -- https://discord.gg/ad7WVB6Bxf
 
 --[[
-            CONTROLS:
+  CONTROLS:
 
-            Left Click: Melee
-            R: you lay on the ground
-            F: Toggle sprint
-            T: Laugh
-            Z: you spin upwards
-            X: (hover the mouse on someone to work) you teleport to that person and they get flung
+  Left Click: Melee
+  R: you lay on the ground
+  F: Toggle sprint
+  T: Laugh
+  Z: you spin upwards
+  X: (hover the mouse on someone to work) you teleport to that person and they get flung
 ]]--
-
-local DummyName = loadstring(game:HttpGet("https://raw.githubusercontent.com/its-asia/animations/main/DummyName.lua", true))()
-
-script = workspace[DummyName]:FindFirstChild('Animate') or workspace[DummyName]:FindFirstChild('Head')
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
 
 local p = LocalPlayer
-local char = p.Character
+
+local char = p.Character.Parent
+local CloneChar = LocalPlayer.Character
+
 local mouse = p:GetMouse()
 local larm = char["Left Arm"]
 local rarm = char["Right Arm"]
@@ -58,11 +55,11 @@ newConnection(mouse.Button1Down:Connect(function()
 		ltt = false
 
 		if game:GetService("Players"):GetPlayerFromCharacter(mouse.Target.Parent) then
-			if mouse.Target.Parent.Name == char.Name or mouse.Target.Parent.Name == DummyName then return end
+			if mouse.Target.Parent == char.Name or mouse.Target.Parent == CloneChar then return end
 			game:GetService("RunService").RenderStepped:Wait()
 			wait(1)
 		elseif game:GetService("Players"):GetPlayerFromCharacter(mouse.Target.Parent.Parent) then
-			if mouse.Target.Parent.Name == char.Name or mouse.Target.Parent.Name == DummyName then return end
+			if mouse.Target.Parent == char.Name or mouse.Target.Parent == CloneChar then return end
 
 			game:GetService("RunService").RenderStepped:Wait()
 			wait(1)
@@ -74,9 +71,6 @@ newConnection(mouse.Button1Down:Connect(function()
 		lt = true
 	end
 end))
-
-
-local CloneChar = workspace[DummyName]
 
 ArtificialHB = Instance.new('BindableEvent')
 ArtificialHB.Name = 'Heartbeat'
@@ -158,14 +152,6 @@ task.spawn(function()
 	until Humanoid.Health < 0.1
 end)
 
-local Tail = CloneChar:FindFirstChild("PlainTail")
-if Tail ~= nil then
-	TailWeld = Tail.AccessoryWeld
-else
-	TailWeld = Instance.new("Weld", workspace)
-end
-
-local TC0 = TailWeld.C0 * CFrame.new(0,0,0.4)
 local Running = false
 
 local MakeTween = function(timetack,easingstyle,easingdirection,repeats,flipflop)
@@ -318,7 +304,6 @@ newConnection(Mouse.KeyDown:Connect(function(key)
 				tors["Left Shoulder"].C0 = tors["Left Shoulder"].C0:Lerp(ls * CF(0,0,0) * CFA(math.rad(10),0,math.rad(-90)),LerpStyle(0.07*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 				tors["Right Hip"].C0 = tors["Right Hip"].C0:Lerp(rh * CF(0,0,0) * CFA(math.rad(0),math.rad(-30 + math.sin(time()*17)*20),math.rad(-10 +  math.sin(time()*16)*10)),LerpStyle(0.1*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 				tors["Left Hip"].C0 = tors["Left Hip"].C0:Lerp(lh * CF(0,0,0) * CFA(math.rad(0),math.rad(0),math.rad(10 +  math.sin(time()*16)*-10)),LerpStyle(0.1*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
-				TailWeld.C0 = TailWeld.C0:Lerp(TC0 * CFrame.Angles(math.rad(-20 + math.sin(-time()*3.83)*-20),math.rad(-math.sin(time()*3.83/2)*24),0),LerpStyle(0.1*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 			end
 			Attacking = false
 		else
@@ -332,7 +317,6 @@ newConnection(Mouse.KeyDown:Connect(function(key)
 					tors["Left Shoulder"].C0 = tors["Left Shoulder"].C0:Lerp(ls * CF(-0.3,0.4+0.4-math.sin(time()*3)*0.2,-0.4 -0.2+math.sin(time()*3)*0.2) * CFA(math.rad(-45)-math.rad(math.sin(time()*3)*12),math.rad(0),math.rad(-180 + 10 +math.cos(time()*3)*5)),LerpStyle(0.1*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 					tors["Right Hip"].C0 = tors["Right Hip"].C0:Lerp(rh * CF(0,0,0) * CFA(math.rad(-math.sin(time()*3)*12),math.rad(math.sin(time()*3)*30),math.rad(-12 - 50 + math.cos(time()*3)*30)),LerpStyle(0.3*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 					tors["Left Hip"].C0 = tors["Left Hip"].C0:Lerp(lh * CF(0,0,0) * CFA(math.rad(-math.sin(time()*3)*12),math.rad(math.sin(time()*3)*30),math.rad(12 + 50 + math.cos(time()*3)*30)),LerpStyle(0.3*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
-					TailWeld.C0 = TailWeld.C0:Lerp(TC0 * CFrame.Angles(math.rad(0),0,math.rad(math.cos(time()*3)*40)),LerpStyle(0.1*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 				end
 				Attacking = false
 			else if key == "f" then
@@ -395,16 +379,12 @@ Humanoid.WalkSpeed = 20
 while true do
 	swait()
 
-	if workspace[DummyName]:FindFirstChild("Animation") and workspace[DummyName].Animation.Value ~= "neko" then
+	if CloneChar:FindFirstChild("Animation") and CloneChar.Animation.Value ~= "neko" then
 		local Camera = workspace.CurrentCamera
 		Humanoid.WalkSpeed = 16
 		Humanoid.JumpPower = 50
 
 		Anim = ""
-
-		if TailWeld.Parent == workspace then
-			TailWeld:Destroy()
-		end
 
 		for _, Connection in pairs(connections) do
 			pcall(function()
@@ -414,16 +394,16 @@ while true do
 		end
 
 		Camera.CameraType = "Custom"
-		Camera.CameraSubject = workspace[DummyName]
+		Camera.CameraSubject = CloneChar
 
 		break
 	end
 
 	hitfloor = rayCast(RootPart.Position, CFrame.new(RootPart.Position, RootPart.Position - Vector3.new(0, 0.5, 0)).lookVector, 4, CloneChar)
-	
+
 	local torvel = (LocalPlayer.Character.Humanoid.MoveDirection * Vector3.new(1, 0, 1)).magnitude
 	local velderp = RootPart.Velocity.y
-	
+
 	if RootPart.Velocity.y > 0 and hitfloor == nil then
 		Anim = "Jumping"
 	elseif RootPart.Velocity.y < 0 and hitfloor == nil then
@@ -433,10 +413,10 @@ while true do
 	elseif torvel > .5 and  hitfloor ~= nil  then
 		Anim = "Walking"
 	end
-	
+
 	local W1 = LocalPlayer.Character.Humanoid.MoveDirection* RootPart.CFrame.LookVector
 	local W2 = LocalPlayer.Character.Humanoid.MoveDirection* RootPart.CFrame.RightVector
-	
+
 	WVA = W1.X+W1.Z
 	RLV = W2.X+W2.Z
 
@@ -462,7 +442,6 @@ while true do
 			tors["Left Shoulder"].C0 = tors["Left Shoulder"].C0:Lerp(ls * CF(0.1,math.cos((time()*3.83))/10,0) * CFA(math.rad(-5)- -math.sin((-time()*3.83))/8.7,math.rad(0),math.rad(-5)-math.cos((-time()*3.83))/8.7),LerpStyle(0.3*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 			tors["Right Hip"].C0 = tors["Right Hip"].C0:Lerp(rh * CF(0,-math.cos((time()*3.83))/8,0) * CFA(math.rad(-4),math.rad(-12),math.rad(8)),LerpStyle(0.4*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 			tors["Left Hip"].C0 = tors["Left Hip"].C0:Lerp(lh * CF(0,-math.cos((time()*3.83))/8,0) * CFA(math.rad(-1),math.rad(7),math.rad(2)),LerpStyle(0.4*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
-			TailWeld.C0 = TailWeld.C0:Lerp(TC0 * CFrame.Angles(math.rad(-20 + math.sin(-time()*3.83)*-20),math.rad(-math.sin(time()*3.83/2)*24),0),LerpStyle(0.1*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 		elseif Anim == "Walking" and Running == false then
 			RLA =  CFrame.new((math.sin((time()*8))/1)*0.5 *WVA,0.3*(1-WVA),(math.sin((time()*8))/1)*0.5 *RLV) * CFrame.Angles((-math.sin((time()*8))/1.5)*RLV,0,(math.sin((time()*8))/1.2)*WVA)
 			LLA =  CFrame.new((math.sin((time()*8))/1)*0.5 *WVA,0.3*(1-WVA),(math.sin((time()*8))/1)*0.5 *RLV) * CFrame.Angles((-math.sin((time()*8))/1.5)*RLV,0,(math.sin((time()*8))/1.2)*WVA)
@@ -472,7 +451,6 @@ while true do
 			tors["Left Shoulder"].C0 = tors["Left Shoulder"].C0:Lerp(ls * CF(-math.sin((time()*8))/4.5,0,0) * CFA(math.rad(-5),-math.sin((time()*8))*0.1 + math.rad(RootPart.RotVelocity.Y*3),-math.sin((time()*8))*0.4),LerpStyle(0.4,Enum.EasingStyle.Back,Enum.EasingDirection.InOut))
 			tors["Right Hip"].C0 = tors["Right Hip"].C0:Lerp(rh * CF(0,(math.cos((time()*8))/8),0) * RLA * CFA(0, (-math.sin((time()*8))*0.5)*WVA,math.rad( -15 + 10 + WVA*15) + -math.rad(math.sin(time() *(8*2))*10)),LerpStyle(0.2,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 			tors["Left Hip"].C0 = tors["Left Hip"].C0:Lerp(lh * CF(0,(-math.cos((time()*8))/8),0) * LLA * CFA(0, (-math.sin((time()*8))*0.5)*WVA,-math.rad( -15 + 10 + WVA*15) + math.rad(math.sin(time() *(8*2))*10)),LerpStyle(0.2,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
-			TailWeld.C0 = TailWeld.C0:Lerp(TC0 * CFrame.Angles(math.rad(-30 + math.sin(-time()*8)*-20),0,-math.rad(-math.sin(time()*8/2)*20 + RLV*2)),LerpStyle(0.1*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 		elseif Anim == "Walking" and Running == true then
 			RLA =  CFrame.new(0,(0.5 + math.cos(time()*15)*0.5)*WVA+ 0.2,(0.5 - math.cos(time()*15)*0.5)*RLV+ 0.2) * CFrame.Angles(0,-math.rad(math.sin(time()*15)*80)*RLV,math.rad(math.sin(time()*15)*80)*WVA)
 			LLA =  CFrame.new(0,(0.5 + math.cos(time()*15)*0.5)*WVA- 0.2,(-0.5 + math.cos(time()*15)*0.5)*RLV- 0.2) * CFrame.Angles(0,-math.rad(math.sin(time()*15)*80)*RLV,math.rad(-math.sin(time()*15)*80)*WVA)
@@ -484,7 +462,6 @@ while true do
 			tors["Left Shoulder"].C0 = tors["Left Shoulder"].C0:Lerp(ls * CF(-0.6+ -math.sin(time()*15)*1,0,-0.6) * LAA * CFA(math.rad(-10 - -math.cos(time()*15*2)*10),0,math.rad(-80)),LerpStyle(0.6,Enum.EasingStyle.Back,Enum.EasingDirection.InOut))
 			tors["Right Hip"].C0 = tors["Right Hip"].C0:Lerp(rh * CF(0.4 + -math.cos(time()*15)*0.9,0,0) * RLA * CFA(0, 0,math.rad(50)),LerpStyle(0.6,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 			tors["Left Hip"].C0 = tors["Left Hip"].C0:Lerp(lh * CF(-0.4+ math.cos(time()*15)*0.9,0,0) * LLA * CFA(0, 0,math.rad(-80)),LerpStyle(0.6,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
-			TailWeld.C0 = TailWeld.C0:Lerp(TC0 * CFrame.new(0,0.7,0.3) * CFrame.Angles(math.rad(-110 + math.sin(-time()*15)*-20),-math.rad(RLV*30),0),LerpStyle(0.1*1,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut))
 		end
 	end
 end
